883709757ee458eed5223f77eba63270ffe27e88
Kangyangky <55124005+Kangyangky@users.noreply.github.com>
2024-03-28 18:01:01 +0800

    Delete docs directory

--- docs/0e6451da-5ae01ca1b6649caee216.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkexample=self.webpackChunkexample||[]).push([[195],{6409:function(e,r,t){t.d(r,{jfD:function(){return f}});var n,l=t(5717),a=t(7294),c=["children"],f=a.forwardRef((function(e,r){var t=e.children,f=(0,l._)(e,c);return a.createElement(l.I,(0,l.a)({width:20,height:20,viewBox:"0 0 20 20",xmlns:"http://www.w3.org/2000/svg",fill:"currentColor",ref:r},f),n||(n=a.createElement("path",{d:"M2 14.8H18V16H2zM2 11.2H18V12.399999999999999H2zM2 7.6H18V8.799999999999999H2zM2 4H18V5.2H2z"})),t)}))}}]);
-//# sourceMappingURL=0e6451da-5ae01ca1b6649caee216.js.map
